970477737d789649d7517bdd3c665ac5d54ccc35
blinkmeoff <blinkmeoff@gmail.com>
2017-10-03 18:33:56 +0300

    Version 1.0.0

---
+++ MAGAHelpers/MAGAHelpers/Support/MAGAHelpers.h
@@ -0,0 +1,19 @@
+//
+//  MAGAHelpers.h
+//  MAGAHelpers
+//
+//  Created by Игорь М. on 06.10.17.
+//  Copyright © 2017 com.MAGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MAGAHelpers.
+FOUNDATION_EXPORT double MAGAHelpersVersionNumber;
+
+//! Project version string for MAGAHelpers.
+FOUNDATION_EXPORT const unsigned char MAGAHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAGAHelpers/PublicHeader.h>
+
+
